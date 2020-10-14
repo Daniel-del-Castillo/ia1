68939af7b24c8d16371f1ff2935257b421fd86fa
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2020-10-14 21:37:18 +0100

    Refactor args code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use crossterm::Result;
 mod grid;
 use grid::Grid;
@@ -6,7 +6,36 @@ mod frontend;
 use frontend::FrontEnd;
 
 fn main() -> Result<()> {
-    let matches = App::new("ia1")
+    let (m, n) = parse_args();
+    let grid = Grid::new(m, n);
+    let mut frontend = FrontEnd::new(grid);
+    frontend.run()
+}
+
+fn parse_args() -> (usize, usize) {
+    let matches = declare_args();
+    let m = matches.value_of("rows").unwrap_or("10");
+    let n = matches.value_of("columns").unwrap_or("10");
+    let m = match m.parse() {
+        Err(_) | Ok(0) => {
+            eprintln!("The -m parameter must be a positive integer");
+            std::process::exit(-1);
+        }
+        Ok(num) => num,
+    };
+    let n = match n.parse() {
+        Err(_) | Ok(0) => {
+            eprintln!("The -n parameter must be a positive integer");
+            std::process::exit(-1);
+        }
+        Ok(num) => num,
+    };
+
+    (m, n)
+}
+
+fn declare_args() -> ArgMatches<'static> {
+    App::new("ia1")
         .arg(
             Arg::with_name("rows")
                 .short("m")
@@ -19,24 +48,5 @@ fn main() -> Result<()> {
                 .takes_value(true)
                 .help("Set the number of initial columns"),
         )
-        .get_matches();
-    let m = matches.value_of("rows").unwrap_or("10");
-    let n = matches.value_of("columns").unwrap_or("10");
-    let m = match m.parse() {
-        Err(_) | Ok(0) => {
-            eprintln!("The -m parameter must be a positive integer");
-            std::process::exit(-1);
-        }
-        Ok(num) => num,
-    };
-    let n = match n.parse() {
-        Err(_) | Ok(0) => {
-            eprintln!("The -m parameter must be a positive integer");
-            std::process::exit(-1);
-        }
-        Ok(num) => num,
-    };
-    let grid = Grid::new(m, n);
-    let mut frontend = FrontEnd::new(grid);
-    frontend.run()
+        .get_matches()
 }
